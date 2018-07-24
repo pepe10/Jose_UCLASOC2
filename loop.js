0b571eb762fa764d1b24736b8d92a3ced700f8bf
Pepe Jimenez <pepejimenez@Pepes-MacBook-Air.local>
2018-07-24 13:20:54 -0700

    Add photos

---
+++ loop.js
@@ -0,0 +1,9 @@
+var animals = ['zebra', 'lion', 'tiger', 'elephant', 'monkey'];
+var professions = ['doctor', 'lawyer', 'programmer', 'ballerina'];
+var students = ['Emma', 'Yang', 'PepeJ', 'PepeV', 'Ethan', 'Alison', 'Nico', 'Brian', 'Uli', 'Caleb', 'Rawad', 'Hassan', 'Yueyang', 'Lyla']
+var numbers = [12, 457, 52, 16];
+var largestAnimalName= animals.filter(animal=>animal.length>4);
+var capitalize= professions.map(profession=>profession.toUpperCase());
+var awesome= students.map(student=>student+'   is awesome');
+var total = numbers.reduce((memo,currentValue)=>memo+currentValue,0);
+var app= student. forEach(function(student){${student}}
